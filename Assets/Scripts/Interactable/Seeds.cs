69ccb1e354129e6a8acdaf478a2df4218b9a29e0
GamBuziya <118631209+GamBuziya@users.noreply.github.com>
2024-02-08 00:58:19 +0200

    Added Day|Night + Campfire + Interactable Wheat

---
+++ Assets/Scripts/Interactable/Seeds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace.Abstract_classes;
+using UnityEngine;
+
+namespace DefaultNamespace.Interactable
+{
+    public class Seeds : global::Interactable
+    {
+        protected override void Interact()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
