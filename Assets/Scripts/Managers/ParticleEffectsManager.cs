fa28119f8db532d61e03b24d7ba5b2ac89a0d4ee
GamBuziya <118631209+GamBuziya@users.noreply.github.com>
2024-05-04 18:43:55 +0300

    Hide Mouse

--- Assets/Scripts/Managers/ParticleEffectsManager.cs
+++ Assets/Scripts/Managers/ParticleEffectsManager.cs
@@ -18,16 +18,11 @@ namespace Managers
         {
             if (_BloodEffect != null)
             {
-                // Створюємо інстанс ефекту крові в місці зіткнення
                 GameObject bloodEffectInstance = Instantiate(_BloodEffect, collisionPoint, Quaternion.identity);
-
-                // Налаштовуємо напрямок ефекту крові від точки зіткнення
-                Vector3 direction = -bloodEffectInstance.transform.forward; // Напрямок вздовж власної осі Z ефекту
-                bloodEffectInstance.transform.forward = direction; // Встановлюємо напрямок ефекту
-
-                // Опціонально: можна додати інші налаштування частинок крові тут
-
-                // Активуємо ефект крові
+                
+                Vector3 direction = -bloodEffectInstance.transform.forward; 
+                bloodEffectInstance.transform.forward = direction;
+                
                 bloodEffectInstance.SetActive(true);
                 Destroy(bloodEffectInstance, 3f);
             }
